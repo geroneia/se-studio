aa4f1dbdf4c799d7bf6980969035d453053870f7
Kristina Vuychich <kozetta2004@yandex.ru>
2021-08-24 16:06:02 +0300

    Завершает стилизацию каталога, добавляет хедер и футер на страницы заказа и доставки

--- build/js/menu.js
+++ build/js/menu.js
@@ -1,1 +1,17 @@
-const menu=()=>{const e=document.querySelector(".page-header"),a=e.querySelector(".main-nav__toggle"),n=document.querySelector(".page-header__main-nav");e.classList.remove("page-header--nav-opened"),a.classList.remove("main-nav__toggle--nav-opened"),n.classList.remove("main-nav--nav-opened"),a.addEventListener("click",function(){e.classList.toggle("page-header--nav-opened"),a.classList.toggle("main-nav__toggle--nav-opened"),n.classList.toggle("main-nav--nav-opened")})};export default menu;
+const menu = () => {
+  const pageHeader = document.querySelector(`.page-header`);
+  const headerToggle = pageHeader.querySelector(`.main-nav__toggle`);
+  const mainNav = document.querySelector(`.page-header__main-nav`);
+
+  pageHeader.classList.remove(`page-header--nav-opened`);
+  headerToggle.classList.remove(`main-nav__toggle--nav-opened`);
+  mainNav.classList.remove(`main-nav--nav-opened`);
+
+  headerToggle.addEventListener(`click`, function () {
+    pageHeader.classList.toggle(`page-header--nav-opened`);
+    headerToggle.classList.toggle(`main-nav__toggle--nav-opened`);
+    mainNav.classList.toggle(`main-nav--nav-opened`);
+  });
+};
+
+export default menu;
