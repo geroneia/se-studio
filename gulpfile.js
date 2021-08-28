930160973b2de42aca7834279eb8070931874a8f
Kristina Vuychich <kozetta2004@yandex.ru>
2021-08-29 00:20:08 +0300

    Заменяет шрифты на ttf. Исправляет фон в О нас в мобильной версии

--- gulpfile.js
+++ gulpfile.js
@@ -98,7 +98,7 @@ gulp.task("js", function () {
 
 gulp.task("copy", function () {
   return gulp.src([
-    "source/fonts/**/*.{woff,woff2}",
+    "source/fonts/**/*.{woff,woff2,ttf}",
     "source/img/**",
     "source/js/**",
     "source//*.ico"
